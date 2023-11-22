13daef822f87bd2f4fcd97c09fb58e724007faa6
Tran Le Minh Hoang <hoangtlm2410@gmail.com>
2023-11-22 23:43:39 +0700

    implement ui of header in main layout

--- src/hooks/useRoutesElement.tsx
+++ src/hooks/useRoutesElement.tsx
@@ -3,12 +3,17 @@ import ProductList from '../pages/ProductList'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import RegisterLayout from '../layouts/RegisterLayout/RegisterLayout'
+import MainLayout from '../layouts/MainLayout'
 
 export default function useRoutesElement() {
   const routeElements = useRoutes([
     {
       path: '/',
-      element: <ProductList />
+      element: (
+        <MainLayout>
+          <ProductList />
+        </MainLayout>
+      )
     },
     {
       path: '/login',
