24f013d0db4d415d68b41f55aeaf7f93ede6ccb4
Tran Le Minh Hoang <hoangtlm2410@gmail.com>
2023-12-17 21:15:52 +0700

    implement product component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,3 +1,72 @@
+import { Link } from 'react-router-dom'
+
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-50009109-bceb1cea39526a7545c21e0b6c4567b2'
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-sm'>
+            [10 Mẫu] Đèn Ngủ Cảm Biến Ánh Sáng, Tự Động Bật Tắt, Tiết Kiệm Năng Lượng, Đèn Trang Trí, Đèn Phòng Ngủ
+            Khogiasi247
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-through max-w-[50%] text-gray-500 truncate'>
+              <span className='text-xs'>₫</span>
+              <span>5000</span>
+            </div>
+            <div className='text-orange truncate ml-1'>
+              <span className='text-xs'>₫</span>
+              <span>2000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-content-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='w-3 h-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>4.66k</span>
+              <span className='ml-1'>Đã bán</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
