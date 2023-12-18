73702ffa14c5385174ef0444656534a08bca95a7
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2023-12-18 14:08:01 +0700

    implement rating component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
-import { Product as Props } from '../../../types/product';
-import { formatCurrency, formatNumberToSocialStyle } from '../../../utils/utils';
+import { Product as Props } from '../../../types/product'
+import { formatCurrency, formatNumberToSocialStyle } from '../../../utils/utils'
+import ProductRating from '../../../components/ProductRating'
 
 interface ProductProps {
   product: Props
 }
 
-export default function Product({product}: ProductProps) {
+export default function Product({ product }: ProductProps) {
   return (
     <Link to='/'>
       <div className='bg-white shadow rounded-sm hover:translate-y-[-0.0625rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
@@ -18,9 +19,7 @@ export default function Product({product}: ProductProps) {
           />
         </div>
         <div className='p-2 overflow-hidden'>
-          <div className='min-h-[2rem] line-clamp-2 text-sm'>
-            {product.name}
-          </div>
+          <div className='min-h-[2rem] line-clamp-2 text-sm'>{product.name}</div>
           <div className='flex items-center mt-3'>
             <div className='line-through max-w-[50%] text-gray-500 truncate'>
               <span className='text-xs'>₫</span>
@@ -32,43 +31,11 @@ export default function Product({product}: ProductProps) {
             </div>
           </div>
           <div className='mt-3 flex items-center justify-content-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg
-                    enableBackground='new 0 0 15 15'
-                    viewBox='0 0 15 15'
-                    x={0}
-                    y={0}
-                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
-                  >
-                    <polygon
-                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeMiterlimit={10}
-                    />
-                  </svg>
-                </div>
-                <svg
-                  enableBackground='new 0 0 15 15'
-                  viewBox='0 0 15 15'
-                  x={0}
-                  y={0}
-                  className='w-3 h-3 fill-current text-gray-300'
-                >
-                  <polygon
-                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeMiterlimit={10}
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
               <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
+              Pr
             </div>
           </div>
         </div>
