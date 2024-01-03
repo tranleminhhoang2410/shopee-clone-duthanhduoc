ed0fdbf91d05c7f28799219ac685e7eae9605f68
Tran Le Minh Hoang <hoangtlm2410@gmail.com>
2024-01-03 22:42:03 +0700

    init api for purchase

---
+++ src/api/purchase.ts
@@ -0,0 +1,18 @@
+import { Purchase, PurchaseListStatus } from '@/types/purchase'
+import { SuccessResponse } from '@/types/utils'
+import http from '@/utils/http'
+
+const URL = 'purchases'
+
+const purchaseApi = {
+  addToCart(body: { product_id: string; buy_count: number }) {
+    return http.post<SuccessResponse<Purchase>>(`${URL}/add-to-cart`, body)
+  },
+  getPurchase(params: { status: PurchaseListStatus }) {
+    return http.get<SuccessResponse<Purchase[]>>(`${URL}`, {
+      params
+    })
+  }
+}
+
+export default purchaseApi
