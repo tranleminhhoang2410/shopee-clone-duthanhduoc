3dd6be6dfedc0f5e7ceaadfb75c46d9b2d9c2e28
Tran Le Minh Hoang <hoangtlm2410@gmail.com>
2024-02-13 10:31:48 +0700

    implement change password

--- src/pages/User/pages/ChangePassword/ChangePassword.tsx
+++ src/pages/User/pages/ChangePassword/ChangePassword.tsx
@@ -1,3 +1,118 @@
+import userApi from '@/api/user'
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import { ErrorResponse } from '@/types/utils'
+import { isAxiosUnprocessableEntityError } from '@/utils/utils'
+import { UserSchema, userSchema } from '@/utils/validation'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+
+type FormData = Pick<UserSchema, 'password' | 'new_password' | 'confirm_password'>
+const profileSchema = userSchema.pick(['password', 'new_password', 'confirm_password'])
 export default function ChangePassword() {
-  return <div>ChangePassword</div>
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setError,
+    reset
+  } = useForm<FormData>({
+    defaultValues: {
+      password: '',
+      new_password: '',
+      confirm_password: ''
+    },
+    resolver: yupResolver(profileSchema)
+  })
+  const updateProfileMutation = useMutation({
+    mutationFn: userApi.updateProfile
+  })
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const res = await updateProfileMutation.mutateAsync(omit(data, ['confirm_password']))
+      toast.success(res.data.message)
+      reset()
+    } catch (error) {
+      if (isAxiosUnprocessableEntityError<ErrorResponse<FormData>>(error)) {
+        const formError = error.response?.data.data
+        if (formError) {
+          Object.keys(formError).forEach((key) => {
+            setError(key as keyof Omit<FormData, 'confirm_password'>, {
+              message: formError[key as keyof Omit<FormData, 'confirm_password'>],
+              type: 'Server'
+            })
+          })
+        }
+      }
+    }
+  })
+
+  return (
+    <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ sơ của tôi</h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      <form className='mr-auto mt-8 max-w-2xl' onSubmit={onSubmit}>
+        <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-grow'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Mật khẩu cũ</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                className='relative'
+                register={register}
+                name='password'
+                type='password'
+                placeholder='Mật khẩu cũ'
+                errorMessage={errors.password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-grow'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Mật khẩu mới</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                className='relative'
+                register={register}
+                name='new_password'
+                type='password'
+                placeholder='Mật khẩu mới'
+                errorMessage={errors.new_password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-grow'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Xác nhận mật khẩu mới</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input
+                classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                className='relative'
+                register={register}
+                name='confirm_password'
+                type='password'
+                placeholder='Xác nhận mật khẩu mới'
+                errorMessage={errors.confirm_password?.message}
+              />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-col flex-wrap sm:flex-grow'>
+            <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'></div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Button
+                className='flex h-9 items-center rounded-sm bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+                type='submit'
+              >
+                Lưu
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 }
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -65,6 +65,15 @@ function testPriceMinMax(this: yup.TestContext<yup.AnyObject>) {
   return price_min !== '' || price_max !== ''
 }
 
+const handleConfirmPasswordYup = (refString: string) => {
+  return yup
+    .string()
+    .required('Nhập lại mật khẩu là bắt buộc')
+    .min(6, 'Độ dài từ 6 - 160 ký tự')
+    .max(160, 'Độ dài từ 6 - 160 ký tự')
+    .oneOf([yup.ref(refString)], 'Nhập lại mật khẩu không khớp')
+}
+
 export const schema = yup.object({
   email: yup
     .string()
@@ -77,12 +86,7 @@ export const schema = yup.object({
     .required('Mật khẩu là bắt buộc')
     .min(6, 'Độ dài từ 6 - 160 ký tự')
     .max(160, 'Độ dài từ 6 - 160 ký tự'),
-  confirm_password: yup
-    .string()
-    .required('Nhập lại mật khẩu là bắt buộc')
-    .min(6, 'Độ dài từ 6 - 160 ký tự')
-    .max(160, 'Độ dài từ 6 - 160 ký tự')
-    .oneOf([yup.ref('password')], 'Nhập lại mật khẩu không khớp'),
+  confirm_password: handleConfirmPasswordYup('password'),
   price_min: yup.string().test({
     name: 'price-not-allowed',
     message: 'Giá không phù hợp',
@@ -102,9 +106,9 @@ export const userSchema = yup.object({
   address: yup.string().max(160, 'Độ dài tối đa là 160 ký tự'),
   avatar: yup.string().max(1000, 'Độ dài tối đa là 1000 ký tự'),
   date_of_birth: yup.date().max(new Date(), 'Hãy chọn một ngày trong quá khứ'),
-  password: schema.fields['password'],
-  newPassword: schema.fields['password'],
-  confirm_password: schema.fields['confirm_password']
+  password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  new_password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  confirm_password: handleConfirmPasswordYup('new_password')
 })
 
 export type Schema = yup.InferType<typeof schema>
