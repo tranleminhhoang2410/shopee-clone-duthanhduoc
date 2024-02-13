f102c0ae179431866437712afeb65d671f9d2287
Tran Le Minh Hoang <hoangtlm2410@gmail.com>
2024-02-13 11:49:11 +0700

    implement history purhcase

--- src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
+++ src/pages/User/pages/HistoryPurchase/HistoryPurchase.tsx
@@ -1,3 +1,107 @@
+import purchaseApi from '@/api/purchase'
+import path from '@/constants/path'
+import { purchasesStatus } from '@/constants/purchase'
+import useQueryParams from '@/hooks/useQueryParams'
+import { PurchaseListStatus } from '@/types/purchase'
+import { formatCurrency, generateNameId } from '@/utils/utils'
+import { useQuery } from '@tanstack/react-query'
+import classNames from 'classnames'
+import { Link, createSearchParams } from 'react-router-dom'
+
+const purchaseTabs = [
+  {
+    status: purchasesStatus.all,
+    name: 'Tất cả'
+  },
+  {
+    status: purchasesStatus.waitForConfirmation,
+    name: 'Chờ xác nhận'
+  },
+  {
+    status: purchasesStatus.waitForGetting,
+    name: 'Chờ lấy hàng'
+  },
+  {
+    status: purchasesStatus.inProgress,
+    name: 'Đang giao'
+  },
+  {
+    status: purchasesStatus.delivered,
+    name: 'Đã giao'
+  },
+  {
+    status: purchasesStatus.cancelled,
+    name: 'Đã hủy'
+  }
+]
 export default function HistoryPurchase() {
-  return <div>HistoryPurchase</div>
+  const queryParams: { status?: string } = useQueryParams()
+  const status: number = Number(queryParams.status) || purchasesStatus.all
+
+  const { data: purchaseInCartData } = useQuery({
+    queryKey: ['purchases', { status }],
+    queryFn: () => purchaseApi.getPurchases({ status: status as PurchaseListStatus })
+  })
+
+  const purchasesInCart = purchaseInCartData?.data.data
+
+  const purchaseTabsLink = purchaseTabs.map((tab) => (
+    <Link
+      key={tab.status}
+      to={{
+        pathname: path.historyPurchase,
+        search: createSearchParams({
+          status: String(tab.status)
+        }).toString()
+      }}
+      className={classNames('flex flex-1 items-center justify-center border-b-2 bg-white py-4 text-center', {
+        'border-b-orange text-orange': status === tab.status,
+        'border-b-black/10 text-gray-900': status !== tab.status
+      })}
+    >
+      {tab.name}
+    </Link>
+  ))
+
+  return (
+    <div>
+      <div className='overflow-x-auto'>
+        <div className='min-w-[700px]'>
+          <div className='sticky top-0 flex rounded-t-sm shadow-sm'>{purchaseTabsLink}</div>
+          <div>
+            {purchasesInCart?.map((purchase) => (
+              <div key={purchase._id} className='mt-4 rounded-sm border-black/10 bg-white p-6 text-gray-800 shadow-sm'>
+                <Link
+                  to={`${path.home}${generateNameId({ name: purchase.product.name, id: purchase.product._id })}`}
+                  className='flex'
+                >
+                  <div className='flex-shrink-0'>
+                    <img src={purchase.product.image} alt={purchase.product.name} className='h-20 w-20 object-cover' />
+                  </div>
+                  <div className='ml-3 flex-grow overflow-hidden'>
+                    <div className='trunccate'>{purchase.product.name}</div>
+                    <div className='mt-3'>{purchase.buy_count}</div>
+                  </div>
+                  <div className='ml-3 flex-shrink-0'>
+                    <span className='truncate text-gray-500 line-through'>
+                      ₫{formatCurrency(purchase.product.price_before_discount)}
+                    </span>
+                    <span className='ml-2 truncate text-orange'>₫{formatCurrency(purchase.product.price)}</span>
+                  </div>
+                </Link>
+                <div className='flex justify-end'>
+                  <div>
+                    <span>Tổng giá tiền</span>
+                    <span className='ml-2 truncate text-orange'>
+                      ₫{formatCurrency(purchase.product.price * purchase.buy_count)}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
